878a467c158bdfa9704faa2f79804a395b796304
Jefferson A <jabarca840@gmail.com>
2024-12-23 11:09:53 -0600

    otrapuerba

--- Proyecto#1_Progra1/Proyecto#1_Progra1.cpp
+++ Proyecto#1_Progra1/Proyecto#1_Progra1.cpp
@@ -1,9 +1,12 @@
 
 #include <iostream>
 using namespace std;
+string keilor; 
 int main()
 {
+    keilor = "Dilan";
     cout << "Hello World12345!\n";
     cout << "Bye World!";
     cout << "Prueba uno";
+    
 }
